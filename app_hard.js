a71ae0a6c30ba468454cc8309d5c4bcbf3652a18
AriadnaNaya <50470799+AriadnaNaya@users.noreply.github.com>
2020-04-05 02:03:05 -0300

    Ejercicio Original
    / Hasta donde llegue con Emma

--- app_hard.js
+++ app_hard.js
@@ -1,31 +1,55 @@
 const http = require('http');
-const respuestas = require ("./response.js")
+const fs = require('fs');
+const archivo = fs.readFileSync('./movies.json', "utf-8");
+const peliculas = JSON.parse(archivo);
 
 http.createServer((req, res) => {
 	res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+	// Route System
 	switch (req.url) {
+		// Home
 		case '/':
-			res.end (respuestas.home.titulo + respuestas.home.totalEnCartelera + respuestas.home.listadoPeliculas + respuestas.home.pieDePagina)
+			let titulo = "Bienvenidos a DH Movies el mejor sitio para encontrar las mejores películas, incluso mucho mejor que Netflix, Cuevana y PopCorn.\n\n";
+			let cantidad = "Total de peliculas en cartelera:" + cantidadDePeliculas(peliculas) + "\n\n";
+			let listado = "";
+			let titulos = peliculas.movies.map(function(obj) {
+				return obj.title;
+			});
+			let ordenadas = titulos.sort();
+			ordenadas.forEach((element) => {
+				listado += element + "\n";
+			});
+			// ME VOY A COMER EMA, DESP LO SIGO VIENDO.
+			
+			// dale yo ahora te mando todo.
+			//igual dejo compartido el codigo aca... mientras tanto. vuelvo de comer y te lo envio si?
+			// SI, TRANK, no te hagas problema
+			//buen finde emma!!
+			res.end(titulo + cantidad + listado);
 			break;
+		// En cartelera
 		case '/en-cartelera':
-			res.end (respuestas.cartelera.titulo + respuestas.cartelera.totalPeliculas + respuestas.cartelera.listaCartelera)
+			res.end('En cartelera');
 			break;
 		case '/mas-votadas':
-			res.end (respuestas.masVotadas.titulo + respuestas.masVotadas.totalMasVotadas + respuestas.masVotadas.ratingPromedioTotal + respuestas.masVotadas.titulosSubSeccion + respuestas.masVotadas.titulosDePeliculas + respuestas.masVotadas.listadoDePeliculas)
+			res.end('Más Votadas');
 			break;
 		case '/sucursales':
-			res.end(respuestas.sucursales.titulo + respuestas.sucursales.totalSalas + respuestas.sucursales.listaSalas);
+			res.end('Sucursales');
 			break;
 		case '/contacto':
-			res.end(respuestas.contacto.titulo + respuestas.contacto.contacto);
+			res.end('Contacto');
 			break;
 		case '/preguntas-frecuentes':
-			res.end(respuestas.preguntasFrecuentes.titulo + respuestas.preguntasFrecuentes.totalPreguntas + respuestas.preguntasFrecuentes.listadoDePreguntas );
+			res.end('Preguntas Frecuentes');
 			break;
 		default:
 			res.end('404 not found')
 	}
+	// ¿? 
 }).listen(3030, 'localhost', () => console.log('Server running in 3030 port'));
 
-
+function cantidadDePeliculas(objeto){ 
+	return String(objeto.total_movies);
+}
 
